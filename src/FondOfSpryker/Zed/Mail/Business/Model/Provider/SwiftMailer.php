df101d5f730e32cbbeb9c126616a74b24502ef76
Daniel Rose <daniel-rose@gmx.de>
2020-06-04 10:39:25 +0200

    phpstan & phpcs fixes

--- src/FondOfSpryker/Zed/Mail/Business/Model/Provider/SwiftMailer.php
+++ src/FondOfSpryker/Zed/Mail/Business/Model/Provider/SwiftMailer.php
@@ -4,15 +4,13 @@ declare(strict_types = 1);
 
 namespace FondOfSpryker\Zed\Mail\Business\Model\Provider;
 
+use FondOfSpryker\Zed\Mail\Dependency\Mailer\MailToMailerInterface;
 use Generated\Shared\Transfer\MailTransfer;
 use Spryker\Zed\Mail\Business\Model\Provider\SwiftMailer as SprykerSwiftMailer;
 use Spryker\Zed\Mail\Business\Model\Renderer\RendererInterface;
-use FondOfSpryker\Zed\Mail\Dependency\Mailer\MailToMailerInterface;
-
-use const FILTER_VALIDATE_EMAIL;
-
-use function is_string;
 use function filter_var;
+use function is_string;
+use const FILTER_VALIDATE_EMAIL;
 
 class SwiftMailer extends SprykerSwiftMailer
 {
@@ -24,7 +22,6 @@ class SwiftMailer extends SprykerSwiftMailer
     /**
      * @param \Spryker\Zed\Mail\Business\Model\Renderer\RendererInterface $renderer
      * @param \FondOfSpryker\Zed\Mail\Dependency\Mailer\MailToMailerInterface $mailer
-     *
      */
     public function __construct(RendererInterface $renderer, MailToMailerInterface $mailer)
     {
@@ -45,9 +42,9 @@ class SwiftMailer extends SprykerSwiftMailer
     /**
      * @param \Generated\Shared\Transfer\MailTransfer $mailTransfer
      *
-     * @return \FondOfSpryker\Zed\Mail\Business\Model\Provider\SwiftMailer
+     * @return $this
      */
-    protected function addBcc(MailTransfer $mailTransfer): SwiftMailer
+    protected function addBcc(MailTransfer $mailTransfer)
     {
         foreach ($mailTransfer->getRecipientsBcc() as $recipientBccTransfer) {
             if (!is_string($recipientBccTransfer->getEmail())) {
