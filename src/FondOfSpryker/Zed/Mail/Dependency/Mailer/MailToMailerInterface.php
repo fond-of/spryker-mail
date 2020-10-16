1e249621837b899baee5f0863994975b00cd55a5
Julian Zimmermann <julian.zimmermann@fondof.de>
2020-10-16 17:32:52 +0200

    - allow to add mail attachments

--- src/FondOfSpryker/Zed/Mail/Dependency/Mailer/MailToMailerInterface.php
+++ src/FondOfSpryker/Zed/Mail/Dependency/Mailer/MailToMailerInterface.php
@@ -15,4 +15,11 @@ interface MailToMailerInterface extends SprykerMailToMailerInterface
      * @return void
      */
     public function addBcc(string $email, ?string $name = null): void;
+
+    /**
+     * @param string $attachment
+     *
+     * @return void
+     */
+    public function addAttachment(string $attachment): void;
 }
